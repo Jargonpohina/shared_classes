3804417f71fb234f375c0ff96a0795dfcddee747
Schamppu <41271610+Schamppu@users.noreply.github.com>
2023-12-10 23:31:11 +0200

    Added new

--- game_state/game_state.g.dart
+++ game_state/game_state.g.dart
@@ -6,33 +6,19 @@ part of 'game_state.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$GameStateImpl _$$GameStateImplFromJson(Map<String, dynamic> json) =>
-    _$GameStateImpl(
-      lobby: json['lobby'] == null
-          ? null
-          : Lobby.fromJson(json['lobby'] as Map<String, dynamic>),
-      playerOne: json['playerOne'] == null
-          ? null
-          : User.fromJson(json['playerOne'] as Map<String, dynamic>),
+_$GameStateImpl _$$GameStateImplFromJson(Map<String, dynamic> json) => _$GameStateImpl(
+      lobby: json['lobby'] == null ? null : Lobby.fromJson(json['lobby'] as Map<String, dynamic>),
+      playerOne: json['playerOne'] == null ? null : User.fromJson(json['playerOne'] as Map<String, dynamic>),
       playerOneScore: json['playerOneScore'] as int? ?? 0,
-      playerTwo: json['playerTwo'] == null
-          ? null
-          : User.fromJson(json['playerTwo'] as Map<String, dynamic>),
+      playerTwo: json['playerTwo'] == null ? null : User.fromJson(json['playerTwo'] as Map<String, dynamic>),
       playerTwoScore: json['playerTwoScore'] as int? ?? 0,
-      playerInTurn: json['playerInTurn'] == null
-          ? null
-          : User.fromJson(json['playerInTurn'] as Map<String, dynamic>),
+      playerInTurn: json['playerInTurn'] == null ? null : User.fromJson(json['playerInTurn'] as Map<String, dynamic>),
       canSlide: json['canSlide'] as bool? ?? false,
-      stones: (json['stones'] as List<dynamic>?)
-              ?.map((e) => StoneAPI.fromJson(e as Map<String, dynamic>))
-              .toList() ??
-          const [],
-      gameState: $enumDecodeNullable(_$StateEnumMap, json['gameState']) ??
-          State.started,
+      stones: (json['stones'] as List<dynamic>?)?.map((e) => StoneAPI.fromJson(e as Map<String, dynamic>)).toList() ?? const [],
+      gameState: $enumDecodeNullable(_$StateEnumMap, json['gameState']) ?? State.started,
     );
 
-Map<String, dynamic> _$$GameStateImplToJson(_$GameStateImpl instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$$GameStateImplToJson(_$GameStateImpl instance) => <String, dynamic>{
       'lobby': instance.lobby,
       'playerOne': instance.playerOne,
       'playerOneScore': instance.playerOneScore,
@@ -48,8 +34,5 @@ const _$StateEnumMap = {
   State.started: 'started',
   State.ended: 'ended',
   State.resolved: 'resolved',
-<<<<<<< HEAD
   State.quitting: 'quitting',
-=======
->>>>>>> 3765a2890ffae2832e3061af09c81bcbfe1cd234
 };
